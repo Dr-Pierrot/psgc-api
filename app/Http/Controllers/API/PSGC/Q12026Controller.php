f3696276a10c4bbd85c56472123dc33340e7298d
Dr-Pierrot <capulongako16@gmail.com>
2026-04-22 23:01:07 +0800

    feature: regions

--- app/Http/Controllers/API/PSGC/Q12026Controller.php
+++ app/Http/Controllers/API/PSGC/Q12026Controller.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\API\PSGC;
 
 use App\Http\Controllers\Controller;
 use App\Models\PSGC\Q12026;
-use Illuminate\Http\Request;
 
 class Q12026Controller extends Controller
 {
@@ -12,5 +11,65 @@ class Q12026Controller extends Controller
     {
         $data = Q12026::paginate(10);
         return response()->json($data);
+    }
+
+    public function geographic_level()
+    {
+        $geographic_level = Q12026::where('geographic_level', '!=', 'NULL')->select('geographic_level')->distinct()->get();
+        return response()->json($geographic_level);
+    }
+
+    public function city_classification()
+    {
+        $city_classification = Q12026::where('geographic_level', 'City')->select('city_classification')->distinct()->get();
+        return response()->json($city_classification);
+    }
+
+    public function income_classification()
+    {
+        $income_classification = Q12026::where('geographic_level', 'City')->select('income_classification')->distinct()->get();
+        return response()->json($income_classification);
+    }
+
+    public function regions()
+    {
+        $regions = Q12026::where('geographic_level', 'Reg')->get();
+        return response()->json($regions);
+    }
+
+    public function region($psgc_code)
+    {
+        $regions = Q12026::where('geographic_level', 'Reg')->where('psgc_code', $psgc_code)->get();
+        return response()->json($regions);
+    }
+
+    public function region_provinces($psgc_code)
+    {
+        $provinces = Q12026::where('geographic_level', 'Prov')->where('region_code', substr($psgc_code, 0, 2))->get();
+        return response()->json($provinces);
+    }
+
+    public function region_cities($psgc_code)
+    {
+        $cities = Q12026::where('geographic_level', 'City')->where('region_code', substr($psgc_code, 0, 2))->get();
+        return response()->json($cities);
+    }
+
+    public function region_municipalities($psgc_code)
+    {
+        $municipalities = Q12026::where('geographic_level', 'Mun')->where('region_code', substr($psgc_code, 0, 2))->get();
+        return response()->json($municipalities);
+    }
+
+    public function region_submunicipalities($psgc_code)
+    {
+        $submunicipalities = Q12026::where('geographic_level', 'SubMun')->where('region_code', substr($psgc_code, 0, 2))->get();
+        return response()->json($submunicipalities);
+    }
+
+    public function region_barangays($psgc_code)
+    {
+        $barangays = Q12026::where('geographic_level', 'Bgy')->where('region_code', substr($psgc_code, 0, 2))->get();
+        return response()->json($barangays);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -11,9 +11,25 @@ use Illuminate\Support\Facades\Route;
 Route::post('register', [AuthenticationController::class, 'register'])->name('api.register');
 Route::post('login', [AuthenticationController::class, 'login'])->name('api.login');
 Route::get('psgc', [Q12026Controller::class, 'index'])->name('api.psgc');
+Route::get('geographic_level', [Q12026Controller::class, 'geographic_level'])->name('api.geographic_level');
+Route::get('city_classification', [Q12026Controller::class, 'city_classification'])->name('api.city_classification');
+Route::get('income_classification', [Q12026Controller::class, 'income_classification'])->name('api.income_classification');
+
+Route::get('regions', [Q12026Controller::class, 'regions'])->name('api.regions');
+Route::get('region/{psgc_code}', [Q12026Controller::class, 'region'])->name('api.region');
+Route::get('region/{psgc_code}/provinces', [Q12026Controller::class, 'region_provinces'])->name('api.region.provinces');
+Route::get('region/{psgc_code}/cities', [Q12026Controller::class, 'region_cities'])->name('api.region.cities');
+Route::get('region/{psgc_code}/municipalities', [Q12026Controller::class, 'region_municipalities'])->name('api.region.municipalities');
+Route::get('region/{psgc_code}/sub-municipalities', [Q12026Controller::class, 'region_submunicipalities'])->name('api.region.sub-municipalities');
+Route::get('region/{psgc_code}/barangays', [Q12026Controller::class, 'region_barangays'])->name('api.region.barangays');
+
+
+
 
 // ------------------ Get Data ----------------------//
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('user', [AuthenticationController::class, 'userInfo'])->name('api.get-user');
+
+
     Route::post('logout', [AuthenticationController::class, 'logOut'])->name('api.logout');
 });
